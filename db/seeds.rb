fccdf39fe7a6acef8d7edd09df349c2b017484cf
github username <github email address>
2021-06-11 21:05:16 -0400

    add user model and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user1 = User.create(name: 'Default User', email: 'defaultuser@gmail.com')
+user2 = User.create(name: 'Test User', email: 'testuser@gmail.com')
