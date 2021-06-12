6db23e0fe309ffe0b8a23638c4f12acca8578132
github username <github email address>
2021-06-12 17:00:00 -0400

    set up before action to find item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,19 +1,38 @@
 class Api::V1::ItemsController < ApplicationController
+    before_action :find_item, only: [:show, :update, :destroy]
 
+    # GET /items
     def index
         items = Item.all
         render json: items
     end
 
+    #POST /items
     def create
         item = Item.new(item_params)
         if item.Save
             render json: item
+        else
+            render json: {errors: item.errors.full_messages}
+        end
     end
 
+    # SHOW /items/:id
+    def show
+        render json: item
+    end
 
+    #PATCH 
+    def update
+        
+    end
 
     private
+
+    def find_item
+        item = Item.find(params[:id])
+    end
+
     def item_params
         params.require(:item).permit(:name, :notes, :url, :weight, :quantity, :price, :purchase, :sent_home, :user_id, :category_id)
     end
