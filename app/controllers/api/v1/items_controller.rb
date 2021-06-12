1340c0f66d3880f5494a03cfdcd270dbefa0af50
github username <github email address>
2021-06-12 16:51:44 -0400

    create index and create items action

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,4 +1,20 @@
 class Api::V1::ItemsController < ApplicationController
 
+    def index
+        items = Item.all
+        render json: items
+    end
 
+    def create
+        item = Item.new(item_params)
+        if item.Save
+            render json: item
+    end
+
+
+
+    private
+    def item_params
+        params.require(:item).permit(:name, :notes, :url, :weight, :quantity, :price, :purchase, :sent_home, :user_id, :category_id)
+    end
 end
