a9366582e86a15440c23a8b3746c372e841d72c6
github username <github email address>
2021-06-13 11:51:25 -0400

    make login route, create sessions controller, enable cors

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get "/api/v1/login", to: "sessions#create"
+
   namespace :api do 
     namespace :v1 do 
       resources :items
+      resources :sessions
     end
   end
 
