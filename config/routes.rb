b28ad0bdeb03d33060fedef56aa7b2daf461a9ef
github username <github email address>
2021-06-13 19:46:03 -0400

    add additional edit to get current user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/api/v1/login", to: "api/v1/sessions#create"
-  post "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+  post "/api/v1/login", to: "api/v1/sessions#create"
 
   namespace :api do 
     namespace :v1 do 
