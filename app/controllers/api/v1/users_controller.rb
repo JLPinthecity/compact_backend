7eabc5c3fbdc0a64f1f99d21cf48bb65b4576f8c
github username <github email address>
2021-06-16 10:00:08 -0400

    lowercase .save and add new to userserializer in create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,8 +3,8 @@ class Api::V1::UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         byebug
-        if @user.Save
-            render json: UserSerializer(@user)
+        if @user.save
+            render json: UserSerializer.new(@user)
         else
             render json: @user.errors, status: :unprocessable_entity
         end
