d90f9e5876614752c15d5d91103058cc68184407
github username <github email address>
2021-06-16 10:56:13 -0400

    log in user on backend when signup on frontend

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,10 +3,11 @@ class Api::V1::UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             render json: UserSerializer.new(@user)
         else
             render json: {
-                error: "Unable to sign in."
+                error: "Unable to sign in. Please try again."
             }
         end
     end
